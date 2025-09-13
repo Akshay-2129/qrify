012d0dbb8e02a348481d0a310312990c6a71b791
Akshay-29 <bingiakshay29@gmail.com>
2025-09-13 16:32:41 +0530

    changed  logic of app

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from logic import create
-import os
 
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -15,7 +15,7 @@ async def index(request: Request):
 
 @app.post("/generate", response_class=HTMLResponse)
 async def generate(request: Request, link: str = Form(...)):
-    qr_path = create(link)  
+    qr_path = create(link)
     qr_path_for_html = "/" + qr_path.replace("\\", "/")
     
     return templates.TemplateResponse(
